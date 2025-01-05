5f584d15281c37e89dd17bdb912ac9433905195c
the7ven <nkwelcorneille7@gmail.com>
2025-01-05 01:07:24 +0000

    restructuration des styles

--- assets/js/panier.js
+++ assets/js/panier.js
@@ -123,3 +123,28 @@ function removeItemFromCart(index) {
 
     location.reload();
 }
+
+
+// Gestion des sous-menus sur mobile
+
+document.querySelector('.mobile-menu-btn').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle('active');
+    });
+
+
+
+document.querySelectorAll('.has-dropdown').forEach(item => {
+    item.addEventListener('click', function(e) {
+        if (window.innerWidth <= 768) {
+            e.preventDefault();
+            this.parentElement.classList.toggle('active');
+        }
+    });
+});
+
+// Fermeture du menu au clic en dehors
+document.addEventListener('click', function(e) {
+    if (window.innerWidth <= 768 && !e.target.closest('nav') && !e.target.closest('.mobile-menu-btn')) {
+        document.querySelector('nav').classList.remove('active');
+    }
+});
